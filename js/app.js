d028967242a992eb31a55b9594935d5b4a27228f
SHakhzodTojiyev <tojiyevshakhzod@gmail.com>
2021-07-11 16:50:40 +0500

    My commit

---
+++ js/app.js
@@ -0,0 +1,24 @@
+//-------------//
+
+const texts = ['Everybody in this country should learn how to program a computer... because it teaches you how to think.'];
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
+
+(function type() {
+
+    if (count === texts.length) {
+        count = 0;
+    }
+    currentText = texts[count];
+    letter = currentText.slice(0, ++index);
+
+    document.querySelector('.typing').textContent = letter;
+    if (letter.length === currentText.length) {
+        count++;
+        index = 0;
+    }
+    setTimeout(type, 200);
+
+})();
