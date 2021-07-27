9e8215c1e7d957f135ca3f13f70bac993ca9c182
SHakhzodTojiyev <tojiyevshakhzod@gmail.com>
2021-07-27 13:42:49 +0500

    change data file

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+import data from "./base.js";
 //-------------//
 
 const texts = ['Everybody in this country should learn how to program a computer... because it teaches you how to think.'];
@@ -22,3 +23,41 @@ let letter = '';
     setTimeout(type, 200);
 
 })();
+
+// RESULT 
+
+const result = document.querySelector("#result");
+
+data.forEach(element => {
+    result.innerHTML  += `
+    <div class="thumb" data-id="${element.data_id}">
+        <img
+          src="${element.image}"
+          alt="${element.data_id}">
+        <h4><a target="_blank" href="${element.link}">Link <i class="fa fa-external-link-square" aria-hidden="${element.aria_hidden}"></i></a></h4>
+    </div>
+    `;
+})
+
+// ELEMENT SHOW
+const buttons = document.querySelectorAll('.filter_li');
+const section = document.querySelectorAll('.thumb');
+
+let values = "All";
+filter(values);
+
+function filter(values) {
+  section.forEach(show => {
+    show.style.display = "none";
+    if (show.getAttribute("data-id") === values || values === "All") {
+      show.style.display = "block";
+    }
+  });
+}
+buttons.forEach(item => {
+  item.addEventListener('click', () => {
+    // Show images
+    let values = item.textContent;
+    filter(values);
+  });
+});
---
+++ js/base.js
@@ -0,0 +1,51 @@
+export default [
+{
+data_id: "Website",
+image: "https://i.ibb.co/h7CtQTm/1.jpg",
+link: "https://abushop.netlify.app/",
+aria_hidden: true
+},
+{
+data_id: "Website",
+image: "https://i.ibb.co/Yb91XdM/2.jpg",
+link: "https://education-online.netlify.app/",
+aria_hidden: true
+},
+{
+data_id: "Components",
+image: "https://i.ibb.co/TMD6JFh/3.jpg",
+link: "https://2l88p.csb.app/",
+aria_hidden: true
+},
+{
+data_id: "Components",
+image: "https://i.ibb.co/hd3wZN8/4.jpg",
+link: "https://89os7.csb.app/",
+aria_hidden: true
+},
+{
+data_id: "Components",
+image: "https://i.ibb.co/G5p5kD4/5.jpg",
+link: "https://0wzzf.csb.app/",
+aria_hidden: true
+},
+{
+data_id: "Web App",
+image: "https://i.ibb.co/tqvYz1M/6.jpg",
+link: "https://fcl5i.csb.app/",
+aria_hidden: true
+},
+{
+data_id: "Web App",
+image: "https://i.ibb.co/841LVKv/7.jpg",
+link: "https://ws81m.csb.app/",
+aria_hidden: true
+},
+{
+data_id: "Website",
+image: "https://i.ibb.co/dBJh3VV/8.jpg",
+link: "https://vrnrg.csb.app/",
+aria_hidden: true
+},
+];
+
