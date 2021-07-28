c83cb1849ea2dd50da43d22166fe5659038f8a29
SHAKHZOD TOJIYEV <68785236+SHakhzodTojiyev@users.noreply.github.com>
2021-07-28 17:57:22 +0500

    Update base.js

--- js/base.js
+++ js/base.js
@@ -37,6 +37,17 @@ link: "https://xgujk.csb.app/",
 aria_hidden: true
 },
 {
+data_id: "Web App",
+image: "https://i.ibb.co/Fz8YNbt/1.png",
+link: "https://xgujk.csb.app/",
+aria_hidden: true
+},{
+data_id: "Web App",
+image: "https://i.ibb.co/mBtpT29/image.png",
+link: "https://5s4xv.csb.app/",
+aria_hidden: true
+},
+{
 data_id: "Website",
 image: "https://i.ibb.co/CbSBq09/image.png",
 link: "https://6mt37.csb.app/",
