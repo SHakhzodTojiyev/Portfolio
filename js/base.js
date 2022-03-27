6ffd7027d014d2eee3e75b5ca9283d4bb71dad11
Shakhzod Tojiyev <68785236+SHakhzodTojiyev@users.noreply.github.com>
2022-03-28 04:05:51 +0500

    Update base.js

--- js/base.js
+++ js/base.js
@@ -1,6 +1,11 @@
 
 export default [
   { 
+    image: "https://i.ibb.co/T1hhkJ0/tc.png",
+    data_id: "Website",
+    link: "https://topconsult.netlify.app"
+  },
+  { 
     image: "https://i.ibb.co/qggpbnV/Xamkor5.png",
     data_id: "Website",
     link: "https://business-invest.netlify.app"
